9998acc0cb23d3020656c5b44629c71de8a5ac29
William Mooney Sloneker <wmsloneker@gmail.com>
2019-08-20 18:48:30 -0400

    Developed Ajax method to successful get array of ten results from GIPHY upon a user click

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,13 +2,24 @@ var searchTerm = "";
 
 var topics = ["BEAR ATTACK", "EXPLOSION", "FIRE", "HURRICANE", "MOB", "PARTY", "SANDSTORM", "TORNADO", "WAR"]
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=LFrOQgSuDNumrxqhkVO0vN7rG16fhxa6&q=" + searchTerm + "&limit=10&lang=en&rating=g"
+for (var i = 0; i < topics.length; i++) {
+    newButton = $("<div>").text(topics[i]).addClass("btn btn-light m-1 chaos-button");
+    $("#buttons-container").append(newButton);
+}
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
+$(".chaos-button").on("click", function () {
+    searchTerm = this.textContent;
 
-.then(function(response){
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=LFrOQgSuDNumrxqhkVO0vN7rG16fhxa6&limit=10";
+
+    console.log(searchTerm);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var results = response.data;
+        console.log(results);
+    });
 
 });
