12e4fea37d866767b1bec09a7cdb8bec91a0ecec
William Mooney Sloneker <wmsloneker@gmail.com>
2019-08-21 13:57:27 -0400

    Developed for loop to iterate through results and create elements to display information in HTML

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 var searchTerm = "";
 
-var topics = ["BEAR ATTACK", "EXPLOSION", "FIRE", "HURRICANE", "MOB", "PARTY", "SANDSTORM", "TORNADO", "WAR"]
+var topics = ["AVALANCHE", "BEAR ATTACK", "EXPLOSION", "FIRE", "HURRICANE", "PARTY", "SANDSTORM", "TORNADO", "WAR"];
 
 for (var i = 0; i < topics.length; i++) {
     newButton = $("<div>").text(topics[i]).addClass("btn btn-light m-1 chaos-button");
@@ -8,18 +8,33 @@ for (var i = 0; i < topics.length; i++) {
 }
 
 $(".chaos-button").on("click", function () {
+    //Redefines the searchTerm variable with the button's text
     searchTerm = this.textContent;
 
+    //Builds the API query URL; logs it to the console for reference
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=LFrOQgSuDNumrxqhkVO0vN7rG16fhxa6&limit=10";
-
     console.log(searchTerm);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
+        //Stores the data in a variable; logs that data to the console for reference
         var results = response.data;
         console.log(results);
+
+        //Empties the results container
+        $("#results-container").empty();
+
+        //Iterates through the results array; displays their images and ratings
+        for (var i = 0; i < results.length; i++) {
+            gifDiv = $("<div>");
+            gifImg = $("<img>").attr("src", results[i].images.original_still.url);
+            gifRating = $("<p>").text("Rating: " + (results[i].rating).toUpperCase());
+            gifDiv.append(gifImg, gifRating);
+            $("#results-container").append(gifDiv);
+        }
+
     });
 
 });
